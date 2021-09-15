a1e89a541eca792533d7f6f726c70ea15f898732
Valentin Popov <kalentinvs@gmail.com>
2021-09-15 20:38:19 +0300

    Made url imports possible

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-'use strict'
 const gutil = require('gulp-util')
 const through = require('through2')
+const request = require('sync-request');
+
 const fs = require('fs')
 
 module.exports = function (componentsUrl) {
 	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file)
-			return
-		}
+		if (file.isNull())
+			return cb(null, file)
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-html-import', 'Streaming not supported'))
-			return
-		}
-
-		const fileReg = /@import\s"(.*)"/gi
+		if (file.isStream())
+			return cb(new gutil.PluginError('gulp-html-import', 'Streaming not supported'))
 
 		let data = file.contents.toString()
-		let dataReplace = data.replace(fileReg, (match, componentName) => {
-            console.log(match + ' --> ' + componentName)
-            return fs.readFileSync(componentsUrl + componentName, {
-                encoding: 'utf8'
-            })
-        })
+		data = data.replace(/@import\s"(.*)"/gi, (match, componentName) => {
+			console.log('@import ' + componentName)
+			return componentName.startsWith('https://') ? request('GET', componentName).getBody() :
+				fs.readFileSync(componentsUrl + componentName, {
+					encoding: 'utf8'
+				})
+		})
 
-		file.contents = new Buffer(dataReplace)
+		file.contents = Buffer.from(data)
 		file.path = gutil.replaceExtension(file.path, '.html')
-		console.log('Import finished.')
 		cb(null, file)
 	})
 }
